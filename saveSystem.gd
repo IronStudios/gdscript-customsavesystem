a8510cbb8ec974865fc24126cc79cefdc975577f
Brandon S. Miller <IronBrandon22@gmail.com>
2020-10-17 16:06:56 -0600

    Updated to Beta-2.0.0
    - Removed Unnecessary Code
    - Fixed 'ready()' Function

--- saveSystem.gd
+++ saveSystem.gd
@@ -1,4 +1,5 @@
 extends Node
+# Beta 2.0.0
 
 export var customSaveSystem = false
 export var mainDir = "user://"
@@ -15,24 +16,26 @@ var currentSystem = {
 		{},
 	],
 	settings=[
-
 	]
 }
-var currentSave = -1
+var currentSave = 0
 
 func _ready():
 	if !customSaveSystem:
-		if loadFile(mainDir+"main",true):
-			currentSystem = loadFile(mainDir+"main")
-		else:
-			saveFile(currentSystem,mainDir+"main")
+		if minSaves > 0:
+			for i in minSaves:
+				if loadFile("save"+str(i+1),true):
+					var newSave = loadFile("save"+str(i+1))
+					if newSave != null:
+						if currentSystem.saves.size() <= i:
+							currentSystem.saves.append(newSave)
+						else:
+							currentSystem.saves[i] = newSave
+					elif currentSystem.saves.size() <= i:
+						currentSystem.saves.append({})
 	elif customSaveSystem:
-		pass # "Custom Save System" goes here <
+		pass # "Your Custom Save-System" goes here <
 	loaded = true
-
-# [ Types ]
-# 0. Variable (get_var)
-# 1. Text (get_as_text)
 
 func changeSave(saveID,saveContents=null,remove=false):
 	if saveContents != null:
@@ -46,26 +49,31 @@ func changeSave(saveID,saveContents=null,remove=false):
 			currentSystem.saves.remove(saveID)
 		return i
 
-func loadFile(dir,exists=false,type=0):
+func loadFile(dir,exists=false):
 	var newFile = File.new()
 	if !exists:
 		if newFile.file_exists(dir):
 			newFile.open(dir,File.READ)
-			if type == 0:
-				return newFile.get_var()
-			if type == 1:
-				return newFile.get_as_text()
+			return newFile.get_var()
 			newFile.close()
 		else:
 			return null
-	if exists:
+	elif exists:
 		if newFile.file_exists(dir):
 			return true
 		else:
 			return false
 
-func saveFile(contents,dir,type=0):
+func saveFile(saveType=0,dir="user://customSave",contents=null):
 	var newFile = File.new()
-	newFile.open(dir,File.WRITE)
-	newFile.store_var(contents)
+	if saveType == 0:
+		newFile.open("user://_save"+str(currentSave+1),File.WRITE)
+		newFile.store_var(currentSystem.saves[currentSave])
+	elif saveType == 1:
+		newFile.open(dir,File.WRITE)
+		newFile.store_var(contents)
+	elif saveType == 2:
+		newFile.open("user://_settings")
+	else:
+		return "saveFile: 101"
 	newFile.close()
